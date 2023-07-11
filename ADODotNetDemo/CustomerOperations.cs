fca49511ebb4c6bf0a39619d6bb0b3492d7d9ddd
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-11 17:24:54 +0530

    [somu]Add:Creating DataBase Using Ado.net C# Program.

---
+++ ADODotNetDemo/CustomerOperations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADODotNetDemo
+{
+    internal class CustomerOperations
+    {
+        public static void CreateDataBase()
+        {
+            SqlConnection sql = new SqlConnection("data Source=(localdb)\\MSSQLLocalDB;initial catalog=master; integrated security=true");
+            try
+            {
+                //writing sql query
+                string query = "Create database ADODotNetDemo";
+                SqlCommand command = new SqlCommand(query, sql);
+                //Opening Connection
+                sql.Open();
+                //Executing the SQL query
+                command.ExecuteNonQuery();
+                //Dispalying a message
+                Console.WriteLine("DataBase Created Successfully");
+                Console.WriteLine("------------------------------");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Smething Went Wrong"+ex);
+            }
+            finally
+            {
+                sql.Close();
+            }
+        }
+    }
+}
---
+++ ADODotNetDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ADODotNetDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose Option to Perform the Operation\n1.Create DataBase\n3.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        CustomerOperations.CreateDataBase();
+                        break;
+                        case 2:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
